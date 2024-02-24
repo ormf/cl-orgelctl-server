010cfab475a96b1dc0efef27c3611e7b9b0f7030
ormf <junk14@selma.hfmdk-frankfurt.de>
2024-02-24 10:44:16 +0100

    added new start-remote-lisp.lisp

---
+++ start-remote-lisp.lisp
@@ -0,0 +1,25 @@
+;;; 
+;;; start-remote-lisp.lisp
+;;;
+;;; **********************************************************************
+;;; Copyright (c) 2024 Orm Finnendahl <orm.finnendahl@selma.hfmdk-frankfurt.de>
+;;;
+;;; Revision history: See git repository.
+;;;
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the Gnu Public License, version 2 or
+;;; later. See https://www.gnu.org/licenses/gpl-2.0.html for the text
+;;; of this agreement.
+;;; 
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;;; GNU General Public License for more details.
+;;;
+;;; **********************************************************************
+
+(ql:quickload :cl-orgelctl-server)
+
+(in-package :cl-orgelctl)
+
+(start-lisp-server)
